b5f759ff61104fb312370eaf299c6e3e7399632e
Hoa Le <lehoa1806@gmail.com>
2021-01-01 23:08:00 +0700

    Introduce Env

--- common/env.py
+++ common/env.py
@@ -2,64 +2,75 @@ import json
 import os
 from typing import Dict
 
-from tools.cipher import CipherHelper
 from common.config import Config
+from tools.cipher import CipherHelper
+from tools.functools import cached_property
+from tools.singleton import Singleton
 
 
-def get_environment_variable(env, default=None, require=False):
-    val = os.getenv(env) or default
-    if val is None and require:
-        raise Exception('Environment variable [{}] not available.'.format(env))
-    return val
+class Env(metaclass=Singleton):
+    @classmethod
+    def get_environment_variable(
+        cls,
+        env: str,
+        default=None,
+        require=False,
+    ) -> str:
+        val = os.getenv(env) or default
+        if val is None and require:
+            raise Exception(f'Environment variable [{env}] not available')
+        return val
 
+    # Credentials
+    def get_credentials(
+        self,
+        creds_code: str
+    ) -> Dict:
+        """
+        SCRAPER_CREDENTIALS: '{username: password}'
+        """
+        encrypted = self.get_environment_variable(creds_code)
+        creds_json = CipherHelper(Config().cipher_key).decrypt(encrypted)
+        try:
+            creds = json.loads(creds_json)
+        except (json.decoder.JSONDecodeError, TypeError):
+            creds = {}
+        return creds if isinstance(creds, Dict) else {}
 
-# Credentials
-def get_credentials(creds_code: str):
-    """
-    SCRAPER_CREDENTIALS: '{username: password}'
-    """
-    encrypted = get_environment_variable(creds_code)
-    creds_json = CipherHelper(Config().cipher_key).decrypt(encrypted)
-    try:
-        creds = json.loads(creds_json)
-    except (json.decoder.JSONDecodeError, TypeError):
-        creds = {}
-    return creds if isinstance(creds, Dict) else {}
+    @cached_property
+    def rakuten_credentials(self):
+        return self.get_credentials('RAKUTEN_CREDS_CODE')
 
+    @cached_property
+    def i3investor_credentials(self):
+        return self.get_credentials('I3INVESTOR_CREDS_CODE')
 
-def get_rakuten_credentials():
-    return get_credentials('RAKUTEN_CREDS_CODE')
+    # Slack
+    @cached_property
+    def slack_token(self):
+        return self.get_environment_variable('FINLP_SLACK_TOKEN')
 
+    # Scraper
+    @cached_property
+    def browser_type(self):
+        """
+        SCRAPER_BROWSER_TYPE: CHROME or FIREFOX
+        """
+        return self.get_environment_variable('SCRAPER_BROWSER_TYPE')
 
-def get_i3investor_credentials():
-    return get_credentials('I3INVESTOR_CREDS_CODE')
+    # PostgreSQL
+    @cached_property
+    def postgresql_host(self):
+        return self.get_environment_variable('POSTGRESQL_HOST')
 
+    @cached_property
+    def postgresql_port(self):
+        return self.get_environment_variable('POSTGRESQL_PORT')
 
-# Slack
-def get_slack_token():
-    return get_environment_variable('FINLP_SLACK_TOKEN')
+    @cached_property
+    def postgresql_database(self):
+        return self.get_environment_variable('POSTGRESQL_DATABASE')
 
-
-# Scraper
-def get_browser_type():
-    """
-    SCRAPER_BROWSER_TYPE: CHROME or FIREFOX
-    """
-    return get_environment_variable('SCRAPER_BROWSER_TYPE')
-
-
-# PostgreSQL
-def get_postgresql_host():
-    return get_environment_variable('POSTGRESQL_HOST')
-
-
-def get_postgresql_port():
-    return get_environment_variable('POSTGRESQL_PORT')
-
-
-def get_postgresql_database():
-    return get_environment_variable('POSTGRESQL_DATABASE')
-
-
-def get_postgresql_credentials():
-    return get_credentials('POSTGRESQL_CREDS_CODE')
+    @cached_property
+    def postgresql_credentials(self):
+        return self.get_credentials('POSTGRESQL_CREDS_CODE')

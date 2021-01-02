3330c3ee49175a4579c62baba971e0864e350b06
Hoa Le <lehoa1806@gmail.com>
2021-01-02 22:49:30 +0700

    Introduce machine package

---
+++ machine/postgres_batch_delete.py
@@ -0,0 +1,27 @@
+from typing import Dict, Generator
+
+from postgresql.database import Database
+from workflow.consumer import Consumer
+
+
+class PostgresBatchDelete(Consumer):
+    def __init__(
+        self,
+        table_name: str,
+        batch_size: int,
+    ) -> None:
+        self.table_name = table_name
+        self.batch_size = batch_size
+        self.bulk_delete: Generator = None  # type: ignore
+
+    def setup(self, item: Dict) -> None:
+        database = Database.load_default_database()
+        table = database.load_table(self.table_name)
+        self.bulk_delete = table.delete_each(self.batch_size)
+        self.bulk_delete.send(None)
+
+    def process(self, item: Dict) -> None:
+        self.bulk_delete.send(item)
+
+    def teardown(self, item: Dict) -> None:
+        self.bulk_delete.send(None)

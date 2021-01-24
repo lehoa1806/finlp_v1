66715d8df9c0f905ab869d3343c293272387b962
Hoa Le <lehoa1806@gmail.com>
2021-01-24 16:40:20 +0700

    Update sleep time in run_tasks

--- news/scripts/run_tasks.py
+++ news/scripts/run_tasks.py
@@ -1,8 +1,8 @@
 import logging
 from argparse import Namespace
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
-from typing import Dict
+from typing import Dict, List
 
 import pytz
 
@@ -63,21 +63,50 @@ class Worker:
         }
 
     @classmethod
-    def get_cooldown(cls, timezone: str) -> int:
-        local_timezone = pytz.timezone(timezone)
-        now = datetime.utcnow()
-        local_now = now.replace(tzinfo=pytz.utc).astimezone(local_timezone)
-        local_year = local_now.year
-        local_month = local_now.month
-        local_day = local_now.day
-        hot_time_start = local_timezone.localize(
-            datetime(local_year, local_month, local_day, 8))
-        hot_time_end = local_timezone.localize(
-            datetime(local_year, local_month, local_day, 19))
-        if hot_time_start < local_now < hot_time_end:
-            return 300
-        else:
-            return 3600
+    def take_a_break(cls, timezones: List[str]) -> None:
+        """
+        00:00 -- sleep(3h) -- 08:00 -- sleep(5s) -- 19:00 --sleep(1h)-- 23:00
+          |                                                               |
+          ---------------------------- sleep(3h) -------------------------
+        :param timezones: List of timezones
+        :return: None
+        """
+        _rested_time = 0
+        _timezones = [pytz.timezone(timezone) for timezone in timezones]
+        while True:
+            sleep(300)
+            now = datetime.utcnow()
+            _nows = [
+                now.replace(tzinfo=pytz.utc).astimezone(_timezone)
+                for _timezone in _timezones
+            ]
+            _date_elements = [
+                {'day': _now.day, 'month': _now.month, 'year': _now.year}
+                for _now in _nows
+            ]
+            _local_dates = [
+                _tz.localize(datetime(_dt['year'], _dt['month'], _dt['day']))
+                for _tz, _dt in zip(_timezones, _date_elements)
+            ]
+            _date_min = min(_local_dates).astimezone(pytz.utc)
+            _date_max = max(_local_dates).astimezone(pytz.utc)
+
+            rest_time_end = working_time_start = _date_min + timedelta(hours=8)
+            working_time_end = night_time_start = _date_max + timedelta(hours=19)
+            night_time_end = rest_time_start = _date_max + timedelta(hours=23)
+
+            is_weekend = all(_dt.weekday() in [5, 6] for _dt in _local_dates)
+
+            if rest_time_start <= now or now <= rest_time_end:
+                _rested_time += 300
+                if _rested_time > 7200:
+                    break
+            elif night_time_start <= now <= night_time_end or is_weekend:
+                _rested_time += 300
+                if _rested_time > 2400:
+                    break
+            elif working_time_start <= now <= working_time_end:
+                break
 
     def malaysia(self):
         malaysia_filter = MalaysiaFilter()
@@ -223,9 +252,7 @@ if __name__ == "__main__":
             worker.malaysia()
             logging.info('Scrapping Vietnam news/announcements')
             worker.vietnam()
-            cooldown = min(worker.get_cooldown(MY_TIMEZONE),
-                           worker.get_cooldown(VN_TIMEZONE))
-            sleep(cooldown)
+            worker.take_a_break([MY_TIMEZONE, VN_TIMEZONE])
         except KeyboardInterrupt:
             logging.info('Stop scrappers')
             break
